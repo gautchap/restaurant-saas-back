b76f547965f693aa56fdef6dda22bccd4c413cbc
gautchap <gautchap+github@outlook.com>
2024-07-13 00:48:03 +0200

    feat : add auth controller and user service

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,22 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import { createUserValidator } from '#validators/auth_validator'
+import { inject } from '@adonisjs/core'
+import UserService from '#services/user_service'
+
+@inject()
+export default class AuthController {
+  constructor(protected userService: UserService) {}
+  async signUp({ request }: HttpContext) {
+    const data = request.all()
+    const payload = await createUserValidator.validate(data)
+
+    const user = await this.userService.signUser({ name: payload.name, email: payload.email })
+    return user
+  }
+  async getInfos({ auth }: HttpContext) {
+    const user = await auth.authenticate()
+    if (user) return user
+
+    return 'No user found'
+  }
+}
---
+++ app/services/user_service.ts
@@ -0,0 +1,16 @@
+import User from '#models/user'
+
+export default class UserService {
+  async signUser({ name, email }: { name: string; email: string }) {
+    const user = await User.firstOrCreate(
+      { email },
+      {
+        name,
+        email,
+      }
+    )
+
+    const token = await User.accessTokens.create(user)
+    return { user, accessToken: token.value!.release() }
+  }
+}
