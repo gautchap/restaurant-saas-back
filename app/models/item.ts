f6d7174e2e936e56905f7151df79c18ccac6c374
gautchap <gautchap+github@outlook.com>
2024-07-13 00:44:50 +0200

    feat: add item model

---
+++ app/models/item.ts
@@ -0,0 +1,44 @@
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, belongsTo, column } from '@adonisjs/lucid/orm'
+import { randomUUID } from 'node:crypto'
+
+export default class Item extends BaseModel {
+  static selfAssignPrimaryKey = true
+
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare type: string
+
+  @column()
+  declare name: string
+
+  @column()
+  declare shape: string
+
+  @column()
+  declare x: number
+
+  @column()
+  declare y: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  @belongsTo(() => User, { localKey: 'userId' })
+  declare user: BelongsTo<typeof User>
+
+  @beforeCreate()
+  static assignUuid(item: Item) {
+    item.id = randomUUID()
+  }
+}
