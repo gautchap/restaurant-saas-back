138ecf0cc969ccb6fde2046242bcfd653c1638b8
gautchap <gautchap+github@outlook.com>
2024-07-13 00:45:04 +0200

    refactor: add auth token to user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,27 +1,18 @@
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import Item from '#models/item'
 import { DateTime } from 'luxon'
-import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
-import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 
-const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
-  uids: ['email'],
-  passwordColumnName: 'password',
-})
-
-export default class User extends compose(BaseModel, AuthFinder) {
+export default class User extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare name: string | null
 
   @column()
   declare email: string
-
-  @column({ serializeAs: null })
-  declare password: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
@@ -29,5 +20,14 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 
-  static accessTokens = DbAccessTokensProvider.forModel(User)
+  static accessTokens = DbAccessTokensProvider.forModel(User, {
+    expiresIn: '30 days',
+    prefix: 'oat_',
+    table: 'auth_access_tokens',
+    type: 'auth_token',
+    tokenSecretLength: 40,
+  })
+
+  @hasMany(() => Item, { foreignKey: 'userId' })
+  declare items: HasMany<typeof Item>
 }
