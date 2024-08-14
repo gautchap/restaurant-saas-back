4150afc66a4ea41aa054b5e37447c3d215bdf89b
gautchap <gautchap+github@outlook.com>
2024-08-14 14:32:13 +0200

    refactor: update routes

--- start/routes.ts
+++ start/routes.ts
@@ -10,6 +10,7 @@
 import router from '@adonisjs/core/services/router'
 import AutoSwagger from 'adonis-autoswagger'
 import swagger from '#config/swagger'
+const StripeController = () => import('#controllers/stripe_controller')
 const BookingsController = () => import('#controllers/bookings_controller')
 const AuthController = () => import('#controllers/auth_controller')
 const ItemsController = () => import('#controllers/items_controller')
@@ -28,6 +29,10 @@ router.delete('items/delete', [ItemsController, 'deleteItem'])
 router.post('bookings/create/public', [BookingsController, 'createBookingWithoutAuth'])
 router.post('bookings/create/private', [BookingsController, 'createBookingWithAuth'])
 router.get('bookings/get', [BookingsController, 'getBookings'])
+
+router.post('webhook/stripe/payment', [StripeController, 'postPayment'])
+router.get('products', [StripeController, 'getProducts'])
+router.get('invoices', [StripeController, 'getInvoices'])
 
 router.get('/swagger', async () => {
   return AutoSwagger.default.docs(router.toJSON(), swagger)
