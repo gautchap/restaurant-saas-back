074758a3d775b48076b7dc99fdf8d8d2331dab4a
gautchap <gautchap+github@outlook.com>
2024-09-27 01:12:11 +0200

    refactor: update email template to match frontend endpoints

--- resources/views/emails/auth_email.ts
+++ resources/views/emails/auth_email.ts
@@ -1,3 +1,3 @@
 export default function sendMailConfirmation(token: string) {
-  return `<a href='http://${process.env.HOST}:3000/api/auth/signup?token=${token}'>Confirm your email address</a>`
+  return `<a href='http://${process.env.HOST}:3000/api/cred/signup?token=${token}'>Confirm your email address</a>`
 }
