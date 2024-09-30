8e2beb2266cca72feece9580c37ecbfbf5047be8
gautchap <gautchap+github@outlook.com>
2024-09-30 21:15:36 +0200

    feat: add redis to sync emit events

--- config/transmit.ts
+++ config/transmit.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from '@adonisjs/transmit'
+import { redis } from '@adonisjs/transmit/transports'
 
 export default defineConfig({
-  pingInterval: false,
-  transport: null,
+  pingInterval: '1m',
+  transport: {
+    driver: redis({
+      host: 'redis-server-cache',
+      port: 6379,
+      keyPrefix: 'transmit:',
+    }),
+  },
 })
