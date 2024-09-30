9a7f83ba47676ed1860629c56f50d47d29fc80a4
gautchap <gautchap+github@outlook.com>
2024-09-30 18:42:35 +0200

    feat: use redis as bus driver to pass cache across multiple instances

--- config/cache.ts
+++ config/cache.ts
@@ -1,6 +1,7 @@
 import { BentoCache, bentostore } from 'bentocache'
 import { fileDriver } from 'bentocache/drivers/file'
 import { memoryDriver } from 'bentocache/drivers/memory'
+import { redisBusDriver } from 'bentocache/drivers/redis'
 
 export const cache = new BentoCache({
   default: 'multiLayer',
@@ -12,6 +13,7 @@ export const cache = new BentoCache({
           directory: './.cache',
           pruneInterval: false,
         })
-      ),
+      )
+      .useBus(redisBusDriver({ connection: { host: 'redis-server-cache', port: 6379 } })),
   },
 })
